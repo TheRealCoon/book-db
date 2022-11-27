a3f040fde115df93e4afcc3a806477d24a4aba66
T.Sanyi <s.truczkai@gmail.com>
2022-11-27 20:55:15 +0100

    implement add in BookDaoJdbc

--- src/main/java/com/codecool/bookdb/model/BookDaoJdbc.java
+++ src/main/java/com/codecool/bookdb/model/BookDaoJdbc.java
@@ -1,6 +1,10 @@
 package com.codecool.bookdb.model;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class BookDaoJdbc implements BookDao{
@@ -10,6 +14,18 @@ public class BookDaoJdbc implements BookDao{
     }
     @Override
     public void add(Book book) {
+        try (Connection con = dataSource.getConnection()) {
+            final String SQL = "INSERT INTO book(author_id, title) VALUES(?, ?);";
+            PreparedStatement ps = con.prepareStatement(SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, book.getAuthor().getId());
+            ps.setString(2, book.getTitle());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            book.setId(rs.getInt(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
