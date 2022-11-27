8dddd2577681ba3ae987da0856aa8981d9509bbf
T.Sanyi <s.truczkai@gmail.com>
2022-11-27 20:40:04 +0100

    implement add AuthorDaoJdbc

--- src/main/java/com/codecool/bookdb/model/AuthorDaoJdbc.java
+++ src/main/java/com/codecool/bookdb/model/AuthorDaoJdbc.java
@@ -1,17 +1,34 @@
 package com.codecool.bookdb.model;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-public class AuthorDaoJdbc implements AuthorDao{
+public class AuthorDaoJdbc implements AuthorDao {
     private final DataSource dataSource;
+
     public AuthorDaoJdbc(DataSource dataSource) {
-    this.dataSource = dataSource;
+        this.dataSource = dataSource;
     }
 
     @Override
     public void add(Author author) {
-
+        try (Connection con = dataSource.getConnection()) {
+            final String SQL = "INSERT INTO author(first_name, last_name, birth_date) VALUES(?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, author.getFirstName());
+            ps.setString(2, author.getLastName());
+            ps.setDate(3, author.getBirthDate());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            author.setId(rs.getInt(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
