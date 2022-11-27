58070e88e4ff01ae5465efe61264f65dff5bcd49
T.Sanyi <s.truczkai@gmail.com>
2022-11-27 19:52:21 +0100

    add AuthorDaoJdbc, BookDaoJdbc

---
+++ src/main/java/com/codecool/bookdb/model/AuthorDaoJdbc.java
@@ -0,0 +1,25 @@
+package com.codecool.bookdb.model;
+
+import java.util.List;
+
+public class AuthorDaoJdbc implements AuthorDao{
+    @Override
+    public void add(Author author) {
+
+    }
+
+    @Override
+    public void update(Author author) {
+
+    }
+
+    @Override
+    public Author get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Author> getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/bookdb/model/BookDaoJdbc.java
@@ -0,0 +1,25 @@
+package com.codecool.bookdb.model;
+
+import java.util.List;
+
+public class BookDaoJdbc implements BookDao{
+    @Override
+    public void add(Book book) {
+
+    }
+
+    @Override
+    public void update(Book book) {
+
+    }
+
+    @Override
+    public Book get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
+}
