02c1d5eb6fe5068f73d5d28a4718fc542042eb9b
T.Sanyi <s.truczkai@gmail.com>
2022-12-04 13:56:49 +0100

    rewrite Main.main()

--- src/main/java/com/codecool/bookdb/Main.java
+++ src/main/java/com/codecool/bookdb/Main.java
@@ -1,6 +1,7 @@
 package com.codecool.bookdb;
 
 import com.codecool.bookdb.manager.BookDbManager;
+import com.codecool.bookdb.view.UserInterface;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -9,22 +10,7 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        BookDbManager bookDbManager = new BookDbManager();
-        try {
-            System.out.println("Connecting to database...");
-            Connection con = bookDbManager.connect().getConnection();
-            System.out.println("Connection OK");
-            final String SQL = "Select * FROM book;";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(SQL);
-            while(rs.next()){
-                System.out.println(rs.getString(3));
-            }
-            con.close();
-            System.out.println("Connection closed");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        UserInterface ui = new UserInterface(System.in, System.out);
+        new BookDbManager(ui).run();
     }
 }
