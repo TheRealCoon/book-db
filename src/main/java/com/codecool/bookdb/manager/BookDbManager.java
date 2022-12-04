7f91fc496e6ce19bd49b2e80523b30b68caccd0d
T.Sanyi <s.truczkai@gmail.com>
2022-12-04 13:56:02 +0100

    add new methods to BookDbManager

--- src/main/java/com/codecool/bookdb/manager/BookDbManager.java
+++ src/main/java/com/codecool/bookdb/manager/BookDbManager.java
@@ -1,7 +1,9 @@
 package com.codecool.bookdb.manager;
 
 import com.codecool.bookdb.model.AuthorDao;
+import com.codecool.bookdb.model.AuthorDaoJdbc;
 import com.codecool.bookdb.model.BookDao;
+import com.codecool.bookdb.model.BookDaoJdbc;
 import com.codecool.bookdb.view.UserInterface;
 import org.postgresql.ds.PGSimpleDataSource;
 
@@ -17,6 +19,7 @@ public class BookDbManager {
     public BookDbManager(UserInterface ui) {
         this.ui = ui;
     }
+
     public DataSource connect() throws SQLException {
         var dataSource = new PGSimpleDataSource();
         dataSource.setServerName("localhost");
@@ -25,5 +28,37 @@ public class BookDbManager {
         dataSource.setPassword(System.getenv("PASS"));
         dataSource.setDatabaseName("books");
         return dataSource;
+    }
+
+    public void run() {
+        try {
+            setup();
+        } catch (SQLException throwables) {
+            System.err.println("Could not connect to the database.");
+            return;
+        }
+        mainMenu();
+    }
+
+    private void setup() throws SQLException {
+        DataSource dataSource = connect();
+        authorDao = new AuthorDaoJdbc(dataSource);
+        bookDao = new BookDaoJdbc(dataSource, authorDao);
+    }
+
+    private void mainMenu() {
+        boolean running = true;
+        while (running) {
+            ui.printTitle("Main Menu");
+            ui.printOption('a', "Authors");
+            ui.printOption('b', "Books");
+            ui.printOption('q', "Quit");
+            switch (ui.choice("abq")) {
+                case 'a' -> new AuthorManager(ui, authorDao).run();
+                case 'b' -> new BookManager(ui, bookDao, authorDao).run();
+                case 'q' -> running = false;
+            }
+        }
+
     }
 }
