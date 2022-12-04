e13a5e9d5371a483aeca1f9b1506142fff5f22a6
T.Sanyi <s.truczkai@gmail.com>
2022-12-04 13:57:13 +0100

    delete unused imports from Main

--- src/main/java/com/codecool/bookdb/Main.java
+++ src/main/java/com/codecool/bookdb/Main.java
@@ -3,11 +3,6 @@ package com.codecool.bookdb;
 import com.codecool.bookdb.manager.BookDbManager;
 import com.codecool.bookdb.view.UserInterface;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface(System.in, System.out);
