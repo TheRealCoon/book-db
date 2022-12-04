8966d367410963e373987e59c259c331776a9119
T.Sanyi <s.truczkai@gmail.com>
2022-12-04 13:53:56 +0100

    add constructor to BookDbManager

--- src/main/java/com/codecool/bookdb/manager/BookDbManager.java
+++ src/main/java/com/codecool/bookdb/manager/BookDbManager.java
@@ -1,11 +1,22 @@
 package com.codecool.bookdb.manager;
 
+import com.codecool.bookdb.model.AuthorDao;
+import com.codecool.bookdb.model.BookDao;
+import com.codecool.bookdb.view.UserInterface;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class BookDbManager {
+
+    UserInterface ui;
+    AuthorDao authorDao;
+    BookDao bookDao;
+
+    public BookDbManager(UserInterface ui) {
+        this.ui = ui;
+    }
     public DataSource connect() throws SQLException {
         var dataSource = new PGSimpleDataSource();
         dataSource.setServerName("localhost");
